977f6d9e787f4f472e4693ffc351cec39635b8ec
Adson Luis Rodrigues de Almeida <1241163887@acd.grupotiradentes.com>
2025-10-13 16:33:12 -0300

    first commit

---
+++ src/main/java/com/pagamentos/projeto_programacao/ProjetoProgramacaoApplication.java
@@ -0,0 +1,13 @@
+package com.pagamentos.projeto_programacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoProgramacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoProgramacaoApplication.class, args);
+	}
+
+}
