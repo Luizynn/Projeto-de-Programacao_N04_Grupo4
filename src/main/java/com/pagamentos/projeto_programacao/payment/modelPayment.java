a8908f96f18d2d570ad2a2b4c2e3a4179b762632
Luiz <luizedccs@gmail.com>
2025-10-20 17:59:29 -0300

    Structuring the modelPayment component

--- src/main/java/com/pagamentos/projeto_programacao/payment/modelPayment.java
+++ src/main/java/com/pagamentos/projeto_programacao/payment/modelPayment.java
@@ -1,4 +1,79 @@
 package com.pagamentos.projeto_programacao.payment;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
 public class modelPayment {
+
+    private String id;
+    private String status;
+    private double ticketValue;
+    private String paymentMethod;
+
+    private static final List<String> VALID_PAYMENT_METHODS = Arrays.asList("cartao de credito", "pix", "cartao de debito");
+    private static final String STATUS_IN_PROGRESS = "Em progresso";
+    private static final String STATUS_PAID = "Pago";
+
+    public String getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public double getTicketValue() {
+        return ticketValue;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setTicketValue(double ticketValue) {
+        if(ticketValue <= 0){
+            throw new IllegalArgumentException("O valor do ticket deve ser positivo.");
+        }
+        this.ticketValue = ticketValue;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        if(paymentMethod == null || !VALID_PAYMENT_METHODS.contains(paymentMethod.toLowerCase())){
+            throw new IllegalArgumentException("Método de pagamento inválido");
+        }
+        this.paymentMethod = paymentMethod;
+    }
+
+    public void generatePayment(double ticketValue, String paymentMethod){
+        if(ticketValue <= 0){
+            throw new IllegalArgumentException("O valor do ticket deve ser positivo");
+        }
+        if(paymentMethod == null || !VALID_PAYMENT_METHODS.contains(paymentMethod.toLowerCase())){
+            throw new IllegalArgumentException("Método de pagamento inválido");
+        }
+
+        this.ticketValue = ticketValue;
+        this.paymentMethod = paymentMethod;
+        this.status = STATUS_IN_PROGRESS;
+
+
+        this.id = UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    public void computeDiscount(double discount){
+        if(discount > 1){
+            discount = discount / 100;
+        }
+        double valueAfterDiscount = this.ticketValue * discount;
+        setTicketValue(this.ticketValue - valueAfterDiscount);
+    }
+
+    public void computePayment(){
+        setStatus(STATUS_PAID);
+    }
 }
