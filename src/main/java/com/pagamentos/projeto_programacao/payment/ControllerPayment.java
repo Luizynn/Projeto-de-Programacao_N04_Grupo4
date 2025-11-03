d910053d3df07368d4361ee2b4d2fa360060aaf7
Luiz Eduardo <luiz.ecardoso@souunit.com.br>
2025-11-03 12:11:28 -0300

    Controller do Payment

--- src/main/java/com/pagamentos/projeto_programacao/payment/ControllerPayment.java
+++ src/main/java/com/pagamentos/projeto_programacao/payment/ControllerPayment.java
@@ -1,14 +1,46 @@
 package com.pagamentos.projeto_programacao.payment;
 
-
+import com.pagamentos.projeto_programacao.payment.DTO.CreatePayment;
+import com.pagamentos.projeto_programacao.payment.DTO.EditPayment;
+import com.pagamentos.projeto_programacao.payment.DTO.ResponsePayment;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/payment")
+@RequestMapping("/api/payments")
 public class ControllerPayment {
 
     @Autowired
     private ServicePayment servicePayment;
+
+    @PostMapping
+    public ResponseEntity<ResponsePayment> create(@RequestBody CreatePayment body) {
+        ResponsePayment newPayment = servicePayment.create(body);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newPayment);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ResponsePayment> editPayment(
+            @PathVariable Long id,
+            @RequestBody EditPayment body) {
+
+        ResponsePayment updatedPayment = servicePayment.editPayment(id, body);
+        return ResponseEntity.ok(updatedPayment);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResponsePayment>> findAll() {
+        List<ResponsePayment> allPayments = servicePayment.findAll();
+        return ResponseEntity.ok(allPayments);
+    }
+
+    @GetMapping("/client/{idClient}")
+    public ResponseEntity<List<ResponsePayment>> findByClientId(@PathVariable Long idClient) {
+        List<ResponsePayment> clientPayments = servicePayment.findByClientId(idClient);
+        return ResponseEntity.ok(clientPayments);
+    }
 }
